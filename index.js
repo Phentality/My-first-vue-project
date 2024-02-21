c0bbe217ff7c72e6fe6c95c5815926caf24f166f
phentality <pheonix09@yandex.ru>
2024-02-21 10:19:31 +0300

    Start

---
+++ index.js
@@ -0,0 +1,50 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+let app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
+    }
+})
+
+let app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+let app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Изучить JavaScript' },
+            { text: 'Изучить Vue' },
+            { text: 'Создать что-нибудь классное' }
+        ]
+    }
+})
+
+let app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+let app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Привет, Vue!'
+    }
+})
